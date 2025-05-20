2ab68a75c908c328cca461d273fa216e081b859e
joaogomes8227 <125942538+JoaoGomes8227@users.noreply.github.com>
2025-05-20 12:34:54 -0300

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ reset }: { reset: () => void }) {
   return (
     <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12">
-      <h2 className="text-xl font-bold">Oh no!</h2>
+      <h2 className="text-xl font-bold">Oh não!</h2>
       <p className="my-2">
         Houve um problema com a nossa loja. Pode ser um problema temporário. Tente novamente mais tarde.
       </p>
